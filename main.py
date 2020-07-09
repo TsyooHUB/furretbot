001e10ba8e35dc994c30c8bbd6e57c126939b38a
tsyoo <tsyookr@gmail.com>
2020-07-09 17:52:03 -0400

    Reorganize command structure

--- main.py
+++ main.py
@@ -4,59 +4,54 @@ import discord
 import os
 import pyscreenshot
 
-token = config.token
-userid = config.userid
-
 client = discord.Client()
 
 
-def process_message(msg):
-    if msg.author is client.user or str(msg.author) != userid:
-        return False
-    if type(msg.channel) == discord.TextChannel:
-        if client.user not in msg.mentions:
-            return False
-    return True
+async def shutdown(message):
+    await message.channel.send("Ok, shutting down. Good bye.")
+    os.system('shutdown -s -t 0')
+
+
+async def turnoff(message):
+    await message.channel.send("Ok, turning myself off. Bye!")
+    exit()
+
+
+async def restart(message):
+    await message.channel.send("Ok, restarting. Be right back!")
+    os.system("shutdown -t 0 -r -f")
+
+
+async def screenshot(message):
+    await message.channel.send("Ok, sending you a screenshot.")
+    img = pyscreenshot.grab()
+    img_name = "screenshots/" + str(datetime.datetime.now()).replace(":", "") + ".png"
+    img.save(img_name)
+    await message.channel.send(file=discord.File(img_name))
+
+cmd_dict = {
+            "shut down": shutdown,
+            "turn off": turnoff,
+            "restart": restart,
+            "screen": screenshot
+            }
 
 
 @client.event
 async def on_message(message):
-    result = process_message(message)
-    if result:
-        content = message.content.lower()
-        extra = ""
-        if "hello" in content or "hey" in content or "hi" in content:
-            extra += "Hey! "
-        if "please" in content:
-            extra += "Of course! "
-        if "thanks" in content:
-            extra += "No problem. "
-        if extra != "":
-            await message.channel.send(extra)
-        if "shut down" in content:
-            await message.channel.send("Ok, shutting down. Good bye.")
-            os.system('shutdown -s -t 0')
-        elif "turn off" in content:
-            await message.channel.send("Ok, turning myself off. Bye!")
-            exit()
-        elif "restart" in content:
-            await message.channel.send("Ok, restarting. Be right back!")
-            os.system("shutdown -t 0 -r -f")
-        elif "screen" in content:
-            await message.channel.send("Ok, sending you a screenshot.")
-            img = pyscreenshot.grab()
-            img_name = "screenshots/"+str(datetime.datetime.now()).replace(":", "")+".png"
-            img.save(img_name)
-            await message.channel.send(file=discord.File(img_name))
-        else:
-            if extra == "":
-                await message.channel.send("Sorry, I couldn't process that command.")
-    elif result is False:
-        await message.channel.send("Don't tell me what to do!")
+    if message.author is client.user or str(message.author.id) != config.userid:
+        return False
+    if type(message.channel) == discord.TextChannel:
+        if client.user not in message.mentions:
+            return False
+    for cmd_name in cmd_dict:
+        if cmd_name in message.content.lower():
+            await cmd_dict[cmd_name](message)
 
 
 @client.event
 async def on_ready():
     print('FurretBot started')
 
-client.run(token)
+
+client.run(config.token)
