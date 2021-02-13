122ab98d5b216759c8f7533006029f01c5bded2d
TsyooHUB <36317069+TsyooHUB@users.noreply.github.com>
2021-02-13 04:03:38 -0500

    update gamble command

--- usercommands.py
+++ usercommands.py
@@ -20,22 +20,22 @@ async def balance(message, command):
 
 
 async def gamble(message, command):
-    reply = message.author.mention
+    response = message.author.mention
     curr_balance = userdetails.get_philcoin_balance(message.author.id, message.author.name)
     try:
         gamble_amt = int(command[1])
         if gamble_amt > curr_balance:
-            reply += " You don't have enough philcoins to play."
+            response += " You don't have enough philcoins to play."
         elif gamble_amt <= 0:
-            reply += " You can't play with that amount of philcoins."
+            response += " You can't play with that amount of philcoins."
         else:
             if random.randint(0, 1) == 0:
                 rng = -1
-                reply += f" Sorry, you lose! You lost {gamble_amt} philcoins."
+                response += f" Sorry, you lose! You lost {gamble_amt} philcoins."
             else:
                 rng = 1
-                reply += f" You won! You gained {gamble_amt} philcoins."
-            userdetails.add_philcoin(message.author.id, rng * gamble_amt)
+                response += f" You won! You gained {gamble_amt} philcoins."
+            userdetails.add_philcoin(message.author.id, message.author,name, rng * gamble_amt)
     except IndexError:
-        reply += " Incorrect usage of gamble command."
-    await message.channel.send(reply)
+        response += " Incorrect usage of gamble command."
+    await message.channel.send(response)
