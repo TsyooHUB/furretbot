138a217cf320b48169d3eb064462e9f8688b3d39
TsyooHUB <36317069+TsyooHUB@users.noreply.github.com>
2021-02-13 21:26:44 -0500

    didnt read

--- pet.py
+++ pet.py
@@ -21,8 +21,8 @@ def generate_iv():
 
 def get_pets(user_id):
     pet_list = "Your pets:\n"
-    return Pet.get(Pet.owner_id == str(user_id))
-    for pet in Pet.get(Pet.owner_id == str(user_id)):
+    query = Pet.select().where(Pet.owner_id == str(user_id))
+    for pet in query:
         pet_list += f"[{pet.name} phil - Level: {pet.level}, Exp: {pet.exp}]\n"
     return pet_list
     
