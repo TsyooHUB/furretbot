3b7a5865302eee249386c512df8cc5b0bd9d8054
TsyooHUB <36317069+TsyooHUB@users.noreply.github.com>
2021-02-13 21:42:53 -0500

    Update pet.py

--- pet.py
+++ pet.py
@@ -7,7 +7,7 @@ import time
 
 def generate_pet(user_id):
     new_name = generate_name()
-    if new_name == null: 
+    if new_name == None: 
         return 
     pet = Pet(owner_id=user_id, name=new_name, iv=generate_iv(), level=1, exp=0)
     pet.save()
