34989d7c40e0a7d34648dd97981955373c0a7404
TsyooHUB <36317069+TsyooHUB@users.noreply.github.com>
2021-02-13 21:31:52 -0500

    blind guess

--- pet.py
+++ pet.py
@@ -3,14 +3,14 @@ from random_word import RandomWords
 from random import randint
 
 
-def generate_pet(user_id):
-    new_name = generate_name()
+async def generate_pet(user_id):
+    await new_name = generate_name()
     pet = Pet(owner_id=user_id, name=new_name, iv=generate_iv(), level=1, exp=0)
     pet.save()
     return new_name
 
 
-def generate_name():
+async def generate_name():
     r = RandomWords()
     return r.get_random_word(hasDictionaryDef="true", includePartOfSpeech="noun, adj", minCorpusCount=10)
 
