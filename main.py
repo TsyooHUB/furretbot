20cdd897109f57ba00105276f38ae8661c073f77
TsyooHUB <36317069+TsyooHUB@users.noreply.github.com>
2021-11-06 11:30:03 -0400

    debug

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ bot = commands.Bot(command_prefix=config.prefix)
 
 for filename in os.listdir("cogs"):
     if filename.endswith(".py"):
+        print(filename)
         bot.load_extension(f"cogs.{filename[:-3]}")
 bot.run(os.environ["token"])
